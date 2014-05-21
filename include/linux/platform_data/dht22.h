fd046f66e3cd010b40e7016c87705696ef013411
Robert Fabian <rfabian@pi-ag.com>
2014-05-21 11:48:14 +0200

    DHT22 driver adjustments

--- include/linux/platform_data/dht22.h
+++ include/linux/platform_data/dht22.h
@@ -16,18 +16,9 @@
  * struct dht22_platform_data - dht22 connectivity info
  * @gpio_data:		no. of gpio to which bidirectional data line is
  *			connected.
- * @gpio_sck:		no. of gpio to which the data clock is connected.
- * @supply_mv:		supply voltage in mv. Overridden by regulator if
- *			available.
  * @checksum:		flag to indicate the checksum should be validated.
- * @no_otp_reload:	flag to indicate no reload from OTP.
- * @low_resolution:	flag to indicate the temp/humidity resolution to use.
  */
 struct dht22_platform_data {
 	int gpio_data;
-	int gpio_sck;
-	int supply_mv;
 	bool checksum;
-	bool no_otp_reload;
-	bool low_resolution;
 };
